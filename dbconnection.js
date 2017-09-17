2d284a0583fe4f662b856492dced9889855aad1a
MarcoGarofalo <mgarofalo.andres19@hotmail.com>
2017-09-17 15:48:04 -0500

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 100, //focus it
     host : 'localhost',
-    user : 'marco77713',
+    user : '',
     password : '',
     database : 'flowcharts'
     
